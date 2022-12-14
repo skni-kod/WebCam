56b9679cbb9258c3ad9ffd58f2cb1852899e8d6b
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2022-12-14 19:48:17 +0100

    Tlumaczenie zmiennych
    Zmienilem nazwy zmiennych, komentarzy itp. na angielski odpowiednik. Przenioslem tez dwie funkcje do oddzielnego pliku

--- dataCollection.py
+++ dataCollection.py
@@ -1,41 +1,17 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
-import math
 import time
+from helpers import fix_height, fix_weight
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-
 offset = 20
 imgSize = 300
-
 folder = "Data/Salut_wolkanski"
 counter = 0
 
-def dostosuj_height(imgWhite, imgSize, w, h, imgCrop):
-     """Funkcja dostosowuje height by byl zawsze taki sam jak imgSize i daje obraz dloni
-      na srodek i zwraca poprawiony obraz"""
-     k = imgSize / h
-     wCal = math.ceil(k * w)
-     imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-     wGap = math.ceil((imgSize - wCal) / 2)
-     imgWhite[:, wGap:wCal + wGap] = imgResize
-     return imgWhite
-
-def dostosuj_weight(imgWhite, imgSize, w, h, imgCrop):
-     """Funkcja dostosowuje weight by byl zawsze taki sam jak imgSize i daje obraz dloni 
-     na srodek i zwraca poprawiony obraz"""
-     k = imgSize / w
-     hCal = math.ceil(k * h)
-     imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-     hGap = math.ceil((imgSize - hCal) / 2)
-     imgWhite[hGap:hCal + hGap, ] = imgResize
-     return imgWhite
-
-
 while True:
-    """Glowna czesc programu"""
     success, img = cap.read()
     hands, img = detector.findHands(img)
     if hands:
@@ -46,19 +22,18 @@ while True:
             imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
             aspectRatio = h / w
             if aspectRatio > 1:
-                imgWhite = dostosuj_height(imgWhite, imgSize, w, h, imgCrop)
+                imgWhite = fix_height(imgWhite, imgSize, w, h, imgCrop)
             else:
-               imgWhite = dostosuj_weight(imgWhite, imgSize, w, h, imgCrop)
-            cv2.imshow("Dlon", imgCrop)
-            cv2.imshow("Dlon dobre wymiary", imgWhite)
+                imgWhite = fix_weight(imgWhite, imgSize, w, h, imgCrop)
+            cv2.imshow("Hand", imgCrop)
+            cv2.imshow("Hand shaped", imgWhite)
         except:
-            print("Zmien polozenie dloni!")
+            print("Change hand position!")
             continue
 
-    cv2.imshow("Kamera", img)
+    cv2.imshow("Camera", img)
     key = cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
         cv2.imwrite(f"{folder}/Image_{time.time()}.jpg", imgWhite)
         print(counter)
-
---
+++ helpers.py
@@ -0,0 +1,22 @@
+import math
+import cv2
+
+def fix_height(imgWhite, imgSize, w, h, imgCrop):
+     """The function adjusts the height to always be the same as the imgSize,
+      gives the image of the hand to the center and returns the fixed image"""
+     k = imgSize / h
+     wCal = math.ceil(k * w)
+     imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+     wGap = math.ceil((imgSize - wCal) / 2)
+     imgWhite[:, wGap:wCal + wGap] = imgResize
+     return imgWhite
+
+def fix_weight(imgWhite, imgSize, w, h, imgCrop):
+     """The function adjusts the weight to always be the same as the imgSize,
+      gives the image of the hand to the center and returns the fixed image"""
+     k = imgSize / w
+     hCal = math.ceil(k * h)
+     imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+     hGap = math.ceil((imgSize - hCal) / 2)
+     imgWhite[hGap:hCal + hGap, ] = imgResize
+     return imgWhite
